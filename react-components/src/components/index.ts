cfd1901352764a5148d0bff9cdb853a510e82b17
Rasskris <rassoshenko.kristina@yandex.ru>
2021-07-30 14:55:07 +0300

    feat: add index files for exports

---
+++ react-components/src/components/index.ts
@@ -0,0 +1,4 @@
+export { Card } from './Card';
+export { SearchBar } from './SearchBar';
+export { CardsContainer } from './CardsContainer';
+export { SearchBarContainer } from './SearchBarContainer';
