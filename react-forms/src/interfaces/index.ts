d187b29cfc936aa58628649d66302d0070398079
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-03 18:12:13 +0300

    feat: add index files for export

---
+++ react-forms/src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export { default as IFormDataState } from './formDataState.interface';
+export { default as IFormAction } from './formAction.inteface';
+export { default as IValidationResult } from './validationResult.inteface';
+export { default as IErrorsForm } from './errorsForm.interface';
