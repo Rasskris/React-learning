d7eaa4f864f6163cb1e4beb4b5250903ec2ec877
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-10 12:26:10 +0300

    feat: add index files to export

---
+++ react-api/src/components/index.ts
@@ -0,0 +1,6 @@
+export { Recipe } from './Recipe';
+export { SearchContainer } from './SearchContainer';
+export { useFetch } from './useFetch';
+export { Pagination } from './Pagination';
+export { PreLoader } from './PreLoader';
+export { RecipesList } from './RecipesList';
