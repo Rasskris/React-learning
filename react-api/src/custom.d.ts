9ca561a17f787ed11e9e27e727346c7c3aae0bb9
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-13 13:06:40 +0300

    feat: add custom definition types

---
+++ react-api/src/custom.d.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* -------------------------------- Documents ------------------------------- */
+
+declare module '*.html' {
+  const document: any;
+  export default document;
+}
+/* --------------------------------- Styles --------------------------------- */
+declare module '*.scss' {
+  const image: any;
+  export default image;
+}
+
+/* --------------------------------- Images --------------------------------- */
+
+declare module '*.gif' {
+  const image: any;
+  export default image;
+}
+
+declare module '*.jpg' {
+  const image: any;
+  export default image;
+}
+
+declare module '*.jpeg' {
+  const image: any;
+  export default image;
+}
+
+declare module '*.png' {
+  const image: any;
+  export default image;
+}
+
+declare module '*.svg' {
+  const image: any;
+  export default image;
+}
