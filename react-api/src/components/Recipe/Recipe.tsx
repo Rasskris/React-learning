712ed4fa43033a2722f7d09b04970df6c7960221
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-13 13:14:56 +0300

    refactor: change recipe component

--- react-api/src/components/Recipe/Recipe.tsx
+++ react-api/src/components/Recipe/Recipe.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Recipe.scss';
 import { IRecipe } from '../../interfaces';
+import { parseUri } from '../../helpers';
 
 const Recipe = ({ recipe }: { recipe: IRecipe }): JSX.Element => {
-  const { label, calories, ingredientLines } = recipe;
+  const { label, calories, ingredientLines, uri } = recipe;
+  const id = parseUri(uri);
+
   return (
     <div className="recipe">
       <img className="recipe__img" src={recipe.image} alt="Recipe" />
@@ -16,6 +20,7 @@ const Recipe = ({ recipe }: { recipe: IRecipe }): JSX.Element => {
           <span className="recipe__info-num">{ingredientLines.length}</span> ingredients
         </span>
       </div>
+      <Link to={`/details/${id}`}>Details...</Link>
     </div>
   );
 };
