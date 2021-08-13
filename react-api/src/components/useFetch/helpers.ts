84d1cd8434f1ea1ecc15a14be7e297c361351cfa
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-13 13:23:39 +0300

    refactor: change helper file at custom hook useFetch

--- react-api/src/components/useFetch/helpers.ts
+++ react-api/src/components/useFetch/helpers.ts
@@ -2,6 +2,11 @@
 import { getLastAndFirstIndex } from '../../helpers';
 import { IQuery } from '../../interfaces';
 
+const APP_ID = process.env.APP_ID;
+const APP_KEY = process.env.APP_KEY;
+const APP_URL = process.env.APP_URL;
+const APP_URL_ID = process.env.APP_URL_ID;
+
 const generateError = (): Error => {
   const textError = 'could not fetch the data for that resource';
   throw new Error(textError);
@@ -17,11 +22,12 @@ const getRequestString = ({
     numOfCurrentPage as number,
     countItemsPerPage as number,
   );
-  const APP_ID = process.env.APP_ID;
-  const APP_KEY = process.env.APP_KEY;
-  const APP_URL = process.env.APP_URL;
 
   return `${APP_URL}?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${searchParameter}&from=${from}&to=${to}`;
 };
 
-export { generateError, getRequestString };
+const getRequestStringWithID = (id: string): string => {
+  return `${APP_URL_ID}/${id}?type=public&app_id=${APP_ID}&app_key=${APP_KEY}`;
+};
+
+export { generateError, getRequestString, getRequestStringWithID };
