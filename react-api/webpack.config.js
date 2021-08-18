2ad03ca2c985476a99689cb4277b0015c6ae4458
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-18 12:30:33 +0300

    style: delete comments

--- react-api/webpack.config.js
+++ react-api/webpack.config.js
@@ -5,7 +5,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
 
-// const CopyPlugin = require('copy-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 const mode = process.env.NODE_ENV || "development";
@@ -25,11 +24,6 @@ const plugins = [
   new HtmlWebpackPlugin({
     template: "./src/index.html",
   }),
-  // new CopyPlugin({
-  //   // patterns: [
-  //   //   { from: 'src/assets', to: 'assets' },
-  //   // ],
-  // }),
 ];
 
 module.exports = {
@@ -84,7 +78,6 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js", ".tsx"],
     modules: [
-      // path.join(__dirname, 'node_modules')
       path.resolve('./src'),
       'node_modules',
   ],
