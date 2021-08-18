6965309f4cfc66b3d5881c266610a94d581578f2
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-18 12:52:25 +0300

    refactor: set text to constants

--- react-api/src/pages/About/About.tsx
+++ react-api/src/pages/About/About.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './About.scss';
 
+const titleText = 'Simply Good - Home cooking Ideas!';
+const descriptionText =
+  'Cook delicious family meals everyday with our recipes. Discover over 2.8 million recipes.';
+
 const About = (): JSX.Element => {
   return (
     <section className="about">
-      <h2 className="about__title">Simply Good - Home cooking Ideas!</h2>
-      <p className="about__description">
-        Cook delicious family meals everyday with our recipes. Discover over 10000 recipes.
-      </p>
+      <h2 className="about__title">{titleText}</h2>
+      <p className="about__description">{descriptionText}</p>
       <div className="about__img" />
     </section>
   );
