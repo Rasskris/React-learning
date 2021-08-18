aad447d5a18c3f8a92ffd59a35faaa2504bb241d
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-18 12:53:19 +0300

    refactor: change home page

--- react-api/src/pages/Home/Home.tsx
+++ react-api/src/pages/Home/Home.tsx
@@ -1,58 +1,56 @@
-import React, { useState } from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+import { useGetRecipesQuery } from '../../services/recipe';
+import { useAppSelector } from '../../hooks';
 import './Home.scss';
-import {
-  SearchContainer,
-  RecipesList,
-  useFetch,
-  Pagination,
-  PreLoader,
-  ErrorMessage,
-} from '../../components';
-import { ISearchParams } from '../../interfaces';
-import { initSearchState } from '../../constants';
+import { SearchContainer, RecipesList, Pagination, PreLoader, ErrorMessage } from '../../components';
+import { getLastAndFirstIndex } from '../../helpers';
+import { Data } from '../../types';
 
 const Home = (): JSX.Element => {
-  const [numOfCurrentPage, setNumOfCurrentPage] = useState(1);
-  const [searchParams, setSearchParams] = useState<ISearchParams>(initSearchState);
+  const currentPage = useAppSelector((state) => state.pagination.currentPage);
+  const { countItemsPerPage, searchString, searchParameter } = useAppSelector((state) => state.searchValues);
 
-  const { countItemsPerPage, searchString, searchParameter } = searchParams;
+  const { firstIndex: from, lastIndex: to } = getLastAndFirstIndex(
+    currentPage as number,
+    countItemsPerPage as number,
+  );
 
-  const changeNumOfCurrentPage = (numOfPage: number) => {
-    console.log('changeNumber', numOfPage);
-    setNumOfCurrentPage(numOfPage);
-  };
-
-  const changeSearchParams = (newSearchParams: Partial<ISearchParams>) => {
-    setSearchParams((prevState) => ({
-      ...prevState,
-      ...newSearchParams,
-    }));
-    console.log('changeSearchParams');
-  };
-
-  const { data, error, isPending } = useFetch({
+  const { data, isError, error, isLoading } = useGetRecipesQuery({
     searchString,
     searchParameter,
-    numOfCurrentPage,
-    countItemsPerPage,
+    from,
+    to,
   });
-  console.log(data);
+
+  const resultOfSearch = (response: Data): JSX.Element | string => {
+    const { more, from: firstIndex, to: lastIndex, count } = response;
+
+    if (more) {
+      return (
+        <>
+          <RecipesList data={response} />
+          <Pagination countItems={Number(count)} />
+        </>
+      );
+    }
+    if (!more && firstIndex === lastIndex) {
+      const errorMessage = 'Sorry, only the first 100 recipes are available';
+      return <ErrorMessage error={errorMessage} />;
+    }
+    const errorMessage = `Sorry we couldn't find any matches for ${searchString} recipes...`;
+    return <ErrorMessage error={errorMessage} />;
+  };
 
   return (
     <div className="home">
-      <SearchContainer changeSearchParams={changeSearchParams} />
-      {isPending && <PreLoader />}
-      {error && <ErrorMessage errorMessage={error} />}
-      {data && <RecipesList data={data} />}
-      {data && (
-        <Pagination
-          countItems={Number(data.count)}
-          countItemsPerPage={countItemsPerPage}
-          numOfCurrentPage={numOfCurrentPage}
-          changeNumOfCurrentPage={changeNumOfCurrentPage}
-        />
+      <SearchContainer />
+      {isLoading && <PreLoader />}
+      {isError && (
+        <ErrorMessage error={(error as SerializedError).message || (error as FetchBaseQueryError).status} />
       )}
+      {data && resultOfSearch(data)}
     </div>
   );
 };
