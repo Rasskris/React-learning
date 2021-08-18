f85b3801eb35d4b7a32aab1949cb7e3c143a5d61
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-18 12:47:14 +0300

    refactor: change recipe-details page

--- react-api/src/pages/RecipeDetails/RecipeDetails.tsx
+++ react-api/src/pages/RecipeDetails/RecipeDetails.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+import { useGetRecipeByIdQuery } from '../../services/recipe';
 import './RecipeDetails.scss';
-import { useFetch, PreLoader, RecipeFull, ErrorMessage } from '../../components';
+import { PreLoader, RecipeFull, ErrorMessage } from '../../components';
 
 const RecipeDetails = (): JSX.Element => {
   const { id } = useParams<{ id: string }>();
-  const { data, isPending, error } = useFetch({ id });
+  const { data, isLoading, isError, error } = useGetRecipeByIdQuery(id);
 
   return (
     <section className="recipeDetails">
-      {isPending && <PreLoader />}
-      {error && <ErrorMessage errorMessage={error} />}
+      {isLoading && <PreLoader />}
+      {isError && (
+        <ErrorMessage error={(error as SerializedError).message || (error as FetchBaseQueryError).status} />
+      )}
       {data && <RecipeFull recipe={data.recipe} />}
     </section>
   );
