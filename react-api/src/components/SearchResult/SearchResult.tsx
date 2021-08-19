a483e4baf4cba8c245e79858eff5ece44eb090e9
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-19 15:31:09 +0300

    feat: add search-result component

---
+++ react-api/src/components/SearchResult/SearchResult.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Pagination } from '../Pagination';
+import { RecipesList } from '../RecipesList';
+import { ErrorMessage } from '../ErrorMessage';
+import { Data } from '../../types';
+
+type SearchResultProps = {
+  response: Data;
+  searchString: string;
+};
+
+const SearchResult = ({ response, searchString }: SearchResultProps): JSX.Element => {
+  const { more, from: firstIndex, to: lastIndex, count } = response;
+
+  if (more) {
+    return (
+      <>
+        <RecipesList data={response} />
+        <Pagination countItems={Number(count)} />
+      </>
+    );
+  }
+  if (!more && firstIndex === lastIndex) {
+    const errorMessage = 'Sorry, only the first 100 recipes are available';
+
+    return <ErrorMessage error={errorMessage} />;
+  }
+
+  const errorMessage = `Sorry we couldn't find any matches for ${searchString} recipes...`;
+
+  return <ErrorMessage error={errorMessage} />;
+};
+
+export default SearchResult;
