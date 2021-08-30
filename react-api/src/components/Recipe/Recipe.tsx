22051745447b613d1a97cb301ca73fcbbc5d45d4
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-30 14:24:38 +0300

    refactor: update recipe component

--- react-api/src/components/Recipe/Recipe.tsx
+++ react-api/src/components/Recipe/Recipe.tsx
@@ -5,25 +5,25 @@ import { IRecipe } from '../../interfaces';
 import { parseUri } from '../../helpers';
 
 const Recipe = ({ recipe }: { recipe: IRecipe }): JSX.Element => {
-  const { label, calories, ingredientLines, uri } = recipe;
+  const { label, calories, ingredientLines, uri, image } = recipe;
   const id = parseUri(uri);
 
   return (
     <div className="recipe">
-      <img className="recipe__img" src={recipe.image} alt="Recipe" />
+      <img className="recipe__img" src={image} alt={label} />
       <p data-testid="title" className="recipe__title">
         {label}
       </p>
       <div className="recipe__info">
         <span className="recipe__info-item calories">
           <span data-testid="calories" className="recipe__info-num">
-            {calories.toFixed(0)}
+            {calories.toFixed(0)}{' '}
           </span>
           calories
         </span>
         <span className="recipe__info-item">
           <span data-testid="ingredients" className="recipe__info-num">
-            {ingredientLines.length}
+            {ingredientLines.length}{' '}
           </span>
           ingredients
         </span>
