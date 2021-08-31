387f434fe42fae2ac1269f3c96fb9a89375bc246
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-31 14:05:40 +0300

    update: delete constant

--- react-forms/src/components/Form/constants.ts
+++ react-forms/src/components/Form/constants.ts
@@ -1,13 +1,3 @@
-export const formFieldNames = {
-  fieldNameFirstName: 'firstName',
-  fieldNameLastName: 'lastName',
-  fieldNameEmail: 'email',
-  fieldNameCountryList: 'country',
-  fieldNameDateOfBirth: 'dateOfBirth',
-  fieldNameContactChoice: 'contactChoice',
-  fieldNameAgreeWithRules: 'agreeWithRules',
-};
-
 export const actionType = {
   CHANGE: 'CHANGE',
   RESET: 'RESET',
