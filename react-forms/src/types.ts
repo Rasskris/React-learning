30e80a00dbcada8d4ddf39a68ae0058617e1de71
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-31 14:04:36 +0300

    feat: add type for field props

--- react-forms/src/types.ts
+++ react-forms/src/types.ts
@@ -1,6 +1,13 @@
-import { SetStateAction } from 'react';
+import { ChangeEventHandler, SetStateAction } from 'react';
 import { IFormDataState } from './interfaces';
 
 export type FormProp = {
   setFormDataArray: React.Dispatch<SetStateAction<IFormDataState[]>>;
+};
+
+export type FieldProps = {
+  handleChange: ChangeEventHandler;
+  value: string;
+  renderErrors: (fieldName: string) => JSX.Element | null;
+  isChecked: boolean | undefined;
 };
