aec8384a49ae91ca6a97e6c0bb422a46acc7c08c
Rasskris <rassoshenko.kristina@yandex.ru>
2021-08-31 16:09:02 +0300

    refactor: move some functions add custom hook

--- react-api/src/pages/Home/Home.tsx
+++ react-api/src/pages/Home/Home.tsx
@@ -2,26 +2,14 @@ import React from 'react';
 import { SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
 import { useGetRecipesQuery } from '../../services';
-import { useAppSelector } from '../../hooks';
+import { useGetQueryParams } from '../../hooks';
 import './Home.scss';
 import { SearchContainer, SearchResult, PreLoader, ErrorMessage } from '../../components';
-import { getLastAndFirstIndex } from '../../helpers';
 
 const Home = (): JSX.Element => {
-  const currentPage = useAppSelector((state) => state.pagination.currentPage);
-  const { countItemsPerPage, searchString, searchParameter } = useAppSelector((state) => state.searchValues);
+  const queryParams = useGetQueryParams();
 
-  const { firstIndex: from, lastIndex: to } = getLastAndFirstIndex(
-    currentPage as number,
-    countItemsPerPage as number,
-  );
-
-  const { data, isError, error, isLoading, isFetching } = useGetRecipesQuery({
-    searchString,
-    searchParameter,
-    from,
-    to,
-  });
+  const { data, isError, error, isLoading, isFetching } = useGetRecipesQuery(queryParams);
 
   if (isLoading || isFetching) {
     return (
@@ -37,7 +25,7 @@ const Home = (): JSX.Element => {
       {isError && (
         <ErrorMessage error={(error as SerializedError).message || (error as FetchBaseQueryError).status} />
       )}
-      {data && <SearchResult response={data} searchString={searchString} />}
+      {data && <SearchResult response={data} />}
     </div>
   );
 };
